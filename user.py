208ec682594c1e4d6942719f9a94cfe6aa5237c6
Maureen <murimimaureen69@gmail.com>
2020-04-19 23:28:23 +0300

    Import our user class

--- user.py
+++ user.py
@@ -1,7 +1,7 @@
 class User:
     """Class that generates new instances of the user
     """
-
+    user_details = [] # Empty user list
     def __init__(self, firstName, lastName, userName, email, password):
         """__init__ method helps us instantiate
         """
