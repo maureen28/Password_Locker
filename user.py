ccf26a4343887b24e5d1c74c15cb925cbadc8024
Maureen <murimimaureen69@gmail.com>
2020-04-19 23:25:49 +0300

    Create an init method for instantaiation

--- user.py
+++ user.py
@@ -1,3 +1,12 @@
 class User:
     """Class that generates new instances of the user
     """
+
+    def __init__(self, firstName, lastName, userName, email, password):
+        """__init__ method helps us instantiate
+        """
+        self.firstName = firstName
+        self.lastName = lastName
+        self.userName = userName
+        self.email = email
+        self.password = password
