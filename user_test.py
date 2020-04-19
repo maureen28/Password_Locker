54c1100f2db44cc61d03cf076f360fda517651a6
Maureen <murimimaureen69@gmail.com>
2020-04-19 23:40:23 +0300

    create set up and add assertEqual to test

--- user_test.py
+++ user_test.py
@@ -1,3 +1,8 @@
-import unittest
+import unittest  # Import unittest module
 import pyperclip
 from user import User
+
+class TestUser(unittest.TestCase):
+    """ Defines test cases for the User class behavior """
+    def setUp(self):
+       self.new_user = User('Mutahi','Kimani','mkimz','mutahi@gmail.com','Mtuh123')
