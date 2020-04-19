bf0a6123b3b123f5a259adc950bebb41cafe7422
Maureen <murimimaureen69@gmail.com>
2020-04-20 01:22:45 +0300

    Add credentials classmethods

--- credentials.py
+++ credentials.py
@@ -4,14 +4,35 @@ class Credential:
     credential_list = []  # Empty credential list
 
     def __init__(self, credentialName, userName, email, password):
-      
+
         self.credentialName = credentialName
         self.userName = userName
         self.email = email
         self.password = password
-        
-     def save_user(self):
+
+    def save_credential(self):
         """ Save our credential object to our user details
         """
-        User.user_details.append(self)
-   
+        Credential.credential_list.append(self)
+
+    def delete_credential(self):
+        Credential.credential_list.remove(self)
+
+    @classmethod
+    def find_by_credentialName(cls, credentialName):
+        for credential in cls.credential_list:
+            if credential.credentialName == credentialName:
+                return credential
+
+    @classmethod
+    def credential_exist(cls, credentialName):
+        """Check and view if the user exists in the user-details
+        """
+        for credential in cls.credential_list:
+            if credential.credentialName == credentialName:
+                return True
+        return False
+
+    @classmethod
+    def display_credential(cls):
+        return cls.credential_list
--- credentials_test.py
+++ credentials_test.py
@@ -2,22 +2,37 @@ import unittest
 import pyperclip
 from credentials import Credential
 
+
 class TestCredentials(unittest.TestCase):
-    
+
     def setUp(self):
         """Set up method to run before each test cases.
         """
-        self.new_credential = Credential('Maureen', 'Nimoh' , 'nimoh@yahoo.com', 'nimx12')
-        
+        self.new_credential = Credential('Maureen', 'Nimoh', 'nimoh@yahoo.com', 'nimx12')
+
     def test_init(self):
         self.assertEqual(self.new_credential.credentialName, 'Maureen')
         self.assertEqual(self.new_credential.userName, 'Nimoh')
         self.assertEqual(self.new_credential.email, 'nimoh@yahoo.com')
         self.assertEqual(self.new_credential.password, 'nimx12')
-        
+
     def test_save_credential(self):
-        self.new_credential.save_credential()  
+
+        self.new_credential.save_credential()
         self.assertEqual(len(Credential.credential_list), 1)
 
+    def save_multiple_credentials(self):
+        self.new_credential.save_credential()
+        test_credential = Credential('Miriam', 'Leo', 'lee@yahoo.com', '12pril')
+        test_credential.save_credential()
+        self.assertEqual(len(Credential.credential_list), 2)
 
-    
+    def tearDown(self):
+        Credential.credential_list = []
+
+    def test_delete_user(self):
+        self.new_credential.save_credential()
+        test_credential = Credential('Miriam', 'Leo', 'lee@yahoo.com', '12pril')
+        test_credential.save_credential()
+        self.new_credential.delete_credential()  # Deleting a credential object
+        self.assertEqual(len(Credential.credential_list), 1)
