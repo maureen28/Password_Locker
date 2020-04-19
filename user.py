603418d98ada77d340496ee7e9f0980629436007
Maureen <murimimaureen69@gmail.com>
2020-04-20 00:02:15 +0300

    Created a save and delete function

--- user.py
+++ user.py
@@ -1,7 +1,8 @@
 class User:
     """Class that generates new instances of the user
     """
-    user_details = [] # Empty user list
+    user_details = []  # Empty user list
+
     def __init__(self, firstName, lastName, userName, email, password):
         """__init__ method helps us instantiate
         """
@@ -10,3 +11,8 @@ class User:
         self.userName = userName
         self.email = email
         self.password = password
+
+    def save_user(self):
+        """ Save our user object to our user details
+        """
+        User.user_details.append(self)
--- user_test.py
+++ user_test.py
@@ -2,7 +2,40 @@ import unittest  # Import unittest module
 import pyperclip
 from user import User
 
+
 class TestUser(unittest.TestCase):
     """ Defines test cases for the User class behavior """
+
     def setUp(self):
-       self.new_user = User('Mutahi','Kimani','mkimz','mutahi@gmail.com','Mtuh123')
+        self.new_user = User('Mutahi', 'Kimani', 'mkimz', 'mutahi@gmail.com', 'Mtuh123')
+
+    def test_init(self):
+        self.assertEqual(self.new_user.firstName, 'Mutahi')
+        self.assertEqual(self.new_user.lastName, 'Kimani')
+        self.assertEqual(self.new_user.userName, 'mkimz')
+        self.assertEqual(self.new_user.email, 'mutahi@gmail.com')
+        self.assertEqual(self.new_user.password, 'Mtuh123')
+
+    def test_save_user(self):
+        self.new_user.save_user() # save the new user
+        self.assertEqual(len(User.user_details),1)
+            
+    def tearDown(self):
+        User.user_details =[]
+        
+    def save_multiple_users(self):
+        self.new_user.save_user()
+        test_user = User('Facebook', 'Moh','nimoh', '@foodie','foodzone')
+        test_user.save_user()
+        self.assertEqual(len(User.user_details),2)
+        
+    def test_delete_user(self):
+        
+    
+        
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
